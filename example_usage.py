24e2ce48bf950daf5a555e1ee2a7a20313e4c030
gitstetter <a.fussstetter@gmx.net>
2020-04-21 15:56:34 +0200

    Added Error handling

--- example_usage.py
+++ example_usage.py
@@ -1,4 +1,4 @@
-from mysql_connect import ConnectToDatabase
+from mysql_connect import ConnectToDatabase, ConnectionError, CredentialsError, SQLError
 
 config = {'host': '127.0.0.1',
            'user': 'USER',
@@ -7,17 +7,34 @@ config = {'host': '127.0.0.1',
 
 
 def write_to_DB(VALUE1: str, VALUE2: str, VALUE3: str) -> None:
-    with ConnectToDatabase(config['dbconfig']) as cursor:
-        _SQL = """insert into TABLE
-                (COL1, COL2, COL3) values
-                (%s, %s, %s)"""
-        cursor.execute(_SQL, (VALUE1, VALUE2, VALUE3))
+    try:
+        with ConnectToDatabase(config['dbconfig']) as cursor:
+            _SQL = """insert into TABLE
+                    (COL1, COL2, COL3) values
+                    (%s, %s, %s)"""
+            cursor.execute(_SQL, (VALUE1, VALUE2, VALUE3))
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+    print('Unhandled Error:', str(err))
 
 def read_from_DB() -> 'list':
-    with ConnectToDatabase(config['dbconfig']) as cursor:
-        _SQL = """select * from TABLE"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    return contents
-
+    try:
+        with ConnectToDatabase(config['dbconfig']) as cursor:
+            _SQL = """select * from TABLE"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        return contents
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('User-id/Password issues. Error:', str(err))
+    except SQLError as err:
+        print('Is your query correct? Error:', str(err))
+    except Exception as err:
+    print('Unhandled Error:', str(err))
 
